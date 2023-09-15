1fd9ca1a31b4c49ef6759b31a2b4af8f94395a1b
Jose Charris <jose.charris@horus-sc.com>
2023-09-15 16:22:03 -0500

    Responsive

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleMenu () {
+    var menuToggle = document.querySelector(".menuToggle");
+    var navbar = document.querySelector(".navbar");
+    menuToggle.classList.toggle("active");
+    navbar.classList.toggle("active");
+}
